89bfd05c63ea1f2faff6f96f487299ac33c78b78
Junior Corman <junior.corman@gmail.com>
2016-12-20 15:43:26 -0500

    show Image

--- xcode.opencv.core/main.cpp
+++ xcode.opencv.core/main.cpp
@@ -7,10 +7,26 @@
 //
 
 #include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 using namespace std;
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
     cout << "Hello, World!\n";
+    
+    Mat image = imread("/Users/junior/WorkSpace/xcode.opencv.core/happy.jpg");
+    
+    if(!image.data){
+        cout <<  "Could not open or find the image" << endl ;
+        return -1;
+    }
+    
+    namedWindow("hola", WINDOW_AUTOSIZE);
+    imshow("hola", image);
+    
+    waitKey(0);
+    
     return 0;
 }

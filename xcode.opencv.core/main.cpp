40ebd7742250d7cf682221c69d425287a4946a09
Junior Corman <junior.corman@gmail.com>
2016-12-20 14:57:15 -0500

    Initial Commit

---
+++ xcode.opencv.core/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode.opencv.core
+//
+//  Created by Junior on 20/12/16.
+//  Copyright © 2016 House. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
